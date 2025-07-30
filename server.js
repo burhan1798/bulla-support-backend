5fba0cb987b49180988f987a8172541c70a93190
Bulla Top Up Center <mdb556122@gmail.com>
2025-07-30 20:37:59 +0600

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.post("/send-otp", async (req, res) => {
-  const { phone } = req.body;
+  const { name, username, phone, password } = req.body;
 
   try {
     const verification = await client.verify.v2
